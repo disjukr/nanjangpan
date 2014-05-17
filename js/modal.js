ddf65f283a2fb07ce6ed86a8776238ae88a84130
JongChan Choi <disjukr@naver.com>
2014-05-18 03:40:34 +0900

    modal base

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+var showModal;
+var hideModal;
+
+;(function () {
+    $modalZone = $('#modal-zone');
+    $overlay = $('.overlay', $modalZone);
+    showModal = function (modal) {
+        $(modal, $modalZone).show();
+        $modalZone.fadeIn(300);
+    };
+    hideModal = function (instantly) {
+        var t = instantly === true ? 0 : 300;
+        $modalZone.fadeOut(t, function () {
+            $('modal:visible').hide();
+        });
+    };
+    hideModal(true);
+    $overlay.click(hideModal);
+})();
